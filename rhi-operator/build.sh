e910b0e156bfc7e02d7f8e9e2c7eb96f0394f6c1
Alexandre Kieling <abkieling@gmail.com>
2020-11-30 11:45:58 -0300

    Changes required to integrate with Cachito

--- rhi-operator/build.sh
+++ rhi-operator/build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-cd /
-git clone https://code.engineering.redhat.com/gerrit/rhi-operator
-cd rhi-operator
-go mod download
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o manager main.go
+cd /remote-source
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o manager main.go
